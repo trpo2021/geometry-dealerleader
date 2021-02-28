358d920cfa67ed670f1e83a460daf46228d9ddfa
Востриков Артем <archy20022@yandex.ru>
2021-02-28 17:10:53 +0700

    Begin of work

--- geometry.c
+++ geometry.c
@@ -1,6 +1,62 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h>
+
+int checkfortruth (char array[])
+{
+    if (array[0] == 'c' || array[0] == 'C')
+    {
+        if (array[1] == 'i' || array[1] == 'I')
+        {
+            if (array[2] == 'r' || array[2] == 'R')
+            {
+                if (array[3] == 'c' || array[3] == 'C')
+                {
+                    if (array[4] == 'l' || array[4] == 'L')
+                    {
+                        if (array[5] == 'e' || array[5] == 'E')
+                            printf("OK");
+                        else
+                            printf("expected: 'circle'");
+                    }
+                    else
+                        printf("expected: 'circle'");
+                }
+                else
+                    printf("expected: 'circle'");
+            }
+            else
+                printf("expected: 'circle'");
+        }
+        else
+            printf("expected: 'circle'");
+    }
+    else
+        printf("expected: 'circle'");
+
+    if (array[6] == ' ' || array[6] == '(')
+    {
+        for(int i = 6; array[i] == ' '; i++)
+        {
+        }
+        i = 0;
+    }
+    else
+        printf("Error at column 7: expected '('");
+return 0;
+}
+
 int main()
 {
-    printf("Привет");
-    return 0;
+// circle(1 1, 1.5)
+    char geometryfigure[200];
+    fgets(geometryfigure, 200, stdin);
+
+    checkfortruth(geometryfigure);
+/*
+    for(int i = 0; i < strlen(geometryfigure) - 1; i++) {
+        printf("%d\n", geometryfigure[i]);
+    }
+*/
+
+return 0;
 }
